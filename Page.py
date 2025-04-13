dfb35402a3fdb8ab84106cb2fbd0e50d375ed328
CartoonVideoTest <gytc163@163.com>
2025-04-13 15:38:48 +0800

    Update Page.py
    Tuning Expression

--- Page.py
+++ Page.py
@@ -21,19 +21,19 @@ def result_show(detail):
             st.image(detail[i+2])
         with col2:
             st.write(detail[i+1])
-            see_num = st.text_input('输入观看集数：',key=f'input{i}')
+            see_num = st.text_input('输入观看集数（输入0是最后一集）：',key=f'input{i}')
+            num=sgu(str(detail[i]))[0]
             
             if st.button("集数获取",key=f'button{i}'):
+                st.write(f"共{num}集")
                 if not see_num.isdigit():
                     try:
                         if int(see_num) <=0 :
                             st.write("集数当然是从“1”开始啦！！！")
                     except:
-                        st.write(f"你TM了个巴子的，集数是数字！整数！！你TM了个巴子的输入{see_num}\n\n小心额锤你")
+                        st.write(f"你TM了个巴子的，集数是数字！整数！！你TM了个巴子的输入“{see_num}”  ？\n\n小心额锤你")
                
                 else:
-                    num=sgu(str(detail[i]))[0]
-                    st.write(f"共{num}集")
                     if see_num != '':
                         if int(see_num) > int(num):
                             st.write(f"你TM了个巴子的，一共才{num}集，你却要看{see_num}集\n\n小心额锤你")
